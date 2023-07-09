b1f394b28d8d7bb7f59cc24b4aebeeb59e69d214
barisAkgul <barisakgul1994@gmail.com>
2023-07-09 17:51:18 +0300

    Added styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "./router";
 
+import "@styles/reset.css";
+import "@styles/global.css";
+import "@styles/variables.css";
+
 function App() {
   return <RouterProvider router={router} />;
 }
