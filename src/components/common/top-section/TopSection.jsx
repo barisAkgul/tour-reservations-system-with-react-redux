a6aab83ce4895b4d1c92535bc89ae15a2cf42c99
barisAkgul <barisakgul1994@gmail.com>
2023-07-13 22:37:00 +0300

    Top section refactoring

--- src/components/common/top-section/TopSection.jsx
+++ src/components/common/top-section/TopSection.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import * as S from "./TopSection.styled";
+
 const TopSection = ({ pageName }) => {
-  <S.TopSection>
-    <img
-      src="https://demo.ovatheme.com/tripgo/wp-content/uploads/2022/07/header-banner-tour-list.jpg"
-      alt=""
-    />
-    <h1 className="top-section-header">{pageName}</h1>
-  </S.TopSection>;
+  return (
+    <S.TopSection>
+      <img
+        src="https://demo.ovatheme.com/tripgo/wp-content/uploads/2022/07/header-banner-tour-list.jpg"
+        alt=""
+      />
+      <h1 className="top-section-header">{pageName}</h1>
+    </S.TopSection>
+  );
 };
 
 export { TopSection };
