10b5fd990f30f27637677956ddf4daf7d996fe05
barisAkgul <barisakgul1994@gmail.com>
2023-08-03 18:48:46 +0300

    Css file changed to styled component

--- src/components/booking-system/personal-info/PersonalInfo.jsx
+++ src/components/booking-system/personal-info/PersonalInfo.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 
 import * as S from "./PersonalInfo.styled";
-import "./styles.css";
+//import "./styles.css";
 
 const PersonalInfo = ({ handleInputChange, formValues }) => {
   return (
-    <section className="personal-info-container">
+    <S.PersonalInfoContainer>
       <form action="#" className="form">
         <div className="column">
           <div className="input-box">
             <label>Full Name</label>
-            <input type="text" placeholder="Enter full name" required />
+            <input
+              type="text"
+              name="fullName"
+              placeholder="Enter full name"
+              required
+              onChange={(e) => handleInputChange(e)}
+            />
           </div>
 
           <div className="input-box">
             <label>Email Address</label>
-            <input type="text" placeholder="Enter email address" required />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter email address"
+              required
+              onChange={(e) => handleInputChange(e)}
+            />
           </div>
         </div>
         <div className="column">
           <div className="input-box">
             <label>Phone Number</label>
-            <input type="number" placeholder="Enter phone number" required />
+            <input
+              type="number"
+              name="phone"
+              placeholder="Enter phone number"
+              required
+              onChange={(e) => handleInputChange(e)}
+            />
           </div>
           <div className="input-box">
             <label>Birth Date</label>
@@ -31,32 +49,300 @@ const PersonalInfo = ({ handleInputChange, formValues }) => {
 
         <div className="input-box address">
           <label>Address</label>
-          <input type="text" placeholder="Enter street address" required />
+          <input
+            type="text"
+            name="adress"
+            placeholder="Enter street address"
+            required
+            onChange={(e) => handleInputChange(e)}
+          />
           <input
             type="text"
             placeholder="Enter street address line 2"
             required
           />
-          <div className="column">
-            <div className="select-box">
-              <select>
-                <option hidden>Country</option>
-                <option>America</option>
-                <option>Japan</option>
-                <option>India</option>
-                <option>Turkey</option>
-              </select>
-            </div>
-            <input type="text" placeholder="Enter your city" required />
-          </div>
-          <div className="column">
-            <input type="text" placeholder="Enter your region" required />
-            <input type="number" placeholder="Enter postal code" required />
-          </div>
         </div>
       </form>
-    </section>
+    </S.PersonalInfoContainer>
   );
 };
 
 export { PersonalInfo };
+
+// import React from "react";
+// import * as S from "./PersonalInfo.styled";
+
+// const PersonalInfo = ({ handleInputChange, formValues }) => {
+//   const validateInputs = () => {
+//     return (
+//       !formValues.fullName ||
+//       !formValues.email ||
+//       !formValues.phone ||
+//       !formValues.address
+//     );
+//   };
+
+//   return (
+//     <S.PersonalInfoContainer>
+//       <form action="#" className="form">
+//         <div className="column">
+//           <div className={`input-box ${!formValues.fullName ? "error" : ""}`}>
+//             <label>Full Name</label>
+//             <input
+//               type="text"
+//               name="fullName"
+//               placeholder="Enter full name"
+//               value={formValues.fullName}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//           </div>
+
+//           <div className={`input-box ${!formValues.email ? "error" : ""}`}>
+//             <label>Email Address</label>
+//             <input
+//               type="text"
+//               name="email"
+//               placeholder="Enter email address"
+//               value={formValues.email}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//           </div>
+//         </div>
+//         <div className="column">
+//           <div className={`input-box ${!formValues.phone ? "error" : ""}`}>
+//             <label>Phone Number</label>
+//             <input
+//               type="number"
+//               name="phone"
+//               placeholder="Enter phone number"
+//               value={formValues.phone}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//           </div>
+//           <div className="input-box">
+//             <label>Birth Date</label>
+//             <input type="date" placeholder="Enter birth date" required />
+//           </div>
+//         </div>
+
+//         <div
+//           className={`input-box address ${!formValues.address ? "error" : ""}`}
+//         >
+//           <label>Address</label>
+//           <input
+//             type="text"
+//             name="address"
+//             placeholder="Enter street address"
+//             value={formValues.address}
+//             onChange={(e) => handleInputChange(e)}
+//             required
+//           />
+//           <input
+//             type="text"
+//             placeholder="Enter street address line 2"
+//             required
+//           />
+//         </div>
+//       </form>
+//     </S.PersonalInfoContainer>
+//   );
+// };
+
+// export { PersonalInfo };
+
+// import React from "react";
+// import * as S from "./PersonalInfo.styled";
+
+// const PersonalInfo = ({ handleInputChange, formValues }) => {
+//   return (
+//     <S.PersonalInfoContainer>
+//       <form action="#" className="form">
+//         <div className="column">
+//           <S.InputBox className={formValues.fullName === "" ? "error" : ""}>
+//             <S.Label>Full Name</S.Label>
+//             <S.Input
+//               type="text"
+//               name="fullName"
+//               placeholder="Enter full name"
+//               value={formValues.fullName}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//           </S.InputBox>
+
+//           <S.InputBox className={formValues.email === "" ? "error" : ""}>
+//             <S.Label>Email Address</S.Label>
+//             <S.Input
+//               type="text"
+//               name="email"
+//               placeholder="Enter email address"
+//               value={formValues.email}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//           </S.InputBox>
+//         </div>
+//         <div className="column">
+//           <S.InputBox className={formValues.phone === "" ? "error" : ""}>
+//             <S.Label>Phone Number</S.Label>
+//             <S.Input
+//               type="number"
+//               name="phone"
+//               placeholder="Enter phone number"
+//               value={formValues.phone}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//           </S.InputBox>
+//           <S.InputBox>
+//             <S.Label>Birth Date</S.Label>
+//             <S.Input type="date" placeholder="Enter birth date" required />
+//           </S.InputBox>
+//         </div>
+
+//         <S.AddressInputBox className={formValues.address === "" ? "error" : ""}>
+//           <S.Label>Address</S.Label>
+//           <S.Input
+//             type="text"
+//             name="address"
+//             placeholder="Enter street address"
+//             value={formValues.address}
+//             onChange={(e) => handleInputChange(e)}
+//             required
+//           />
+//           <S.Input
+//             type="text"
+//             placeholder="Enter street address line 2"
+//             required
+//           />
+//         </S.AddressInputBox>
+//       </form>
+//     </S.PersonalInfoContainer>
+//   );
+// };
+
+// export { PersonalInfo };
+
+// import React, { useState } from "react";
+// import * as S from "./PersonalInfo.styled";
+
+// const PersonalInfo = ({ handleInputChange, formValues }) => {
+//   const [fullNameError, setFullNameError] = useState(false);
+//   const [emailError, setEmailError] = useState(false);
+//   const [phoneError, setPhoneError] = useState(false);
+//   const [addressError, setAddressError] = useState(false);
+
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+
+//     // Validation
+//     if (!formValues.fullName) {
+//       setFullNameError(true);
+//     } else {
+//       setFullNameError(false);
+//     }
+
+//     if (!formValues.email) {
+//       setEmailError(true);
+//     } else {
+//       setEmailError(false);
+//     }
+
+//     if (!formValues.phone) {
+//       setPhoneError(true);
+//     } else {
+//       setPhoneError(false);
+//     }
+
+//     if (!formValues.address) {
+//       setAddressError(true);
+//     } else {
+//       setAddressError(false);
+//     }
+//   };
+
+//   return (
+//     <S.PersonalInfoContainer>
+//       <form onSubmit={handleFormSubmit} className="form">
+//         <div className="column">
+//           <S.InputBox className={fullNameError ? "error" : ""}>
+//             <S.Label>Full Name</S.Label>
+//             <S.Input
+//               type="text"
+//               name="fullName"
+//               placeholder="Enter full name"
+//               value={formValues.fullName}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//             {fullNameError && (
+//               <S.ErrorMessage>Please enter your full name.</S.ErrorMessage>
+//             )}
+//           </S.InputBox>
+
+//           <S.InputBox className={emailError ? "error" : ""}>
+//             <S.Label>Email Address</S.Label>
+//             <S.Input
+//               type="text"
+//               name="email"
+//               placeholder="Enter email address"
+//               value={formValues.email}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//             {emailError && (
+//               <S.ErrorMessage>Please enter your email address.</S.ErrorMessage>
+//             )}
+//           </S.InputBox>
+//         </div>
+//         <div className="column">
+//           <S.InputBox className={phoneError ? "error" : ""}>
+//             <S.Label>Phone Number</S.Label>
+//             <S.Input
+//               type="number"
+//               name="phone"
+//               placeholder="Enter phone number"
+//               value={formValues.phone}
+//               onChange={(e) => handleInputChange(e)}
+//               required
+//             />
+//             {phoneError && (
+//               <S.ErrorMessage>Please enter your phone number.</S.ErrorMessage>
+//             )}
+//           </S.InputBox>
+//           <S.InputBox>
+//             <S.Label>Birth Date</S.Label>
+//             <S.Input type="date" placeholder="Enter birth date" required />
+//           </S.InputBox>
+//         </div>
+
+//         <S.AddressInputBox className={addressError ? "error" : ""}>
+//           <S.Label>Address</S.Label>
+//           <S.Input
+//             type="text"
+//             name="address"
+//             placeholder="Enter street address"
+//             value={formValues.address}
+//             onChange={(e) => handleInputChange(e)}
+//             required
+//           />
+//           <input
+//             type="text"
+//             placeholder="Enter street address line 2"
+//             required
+//           />
+//           {addressError && (
+//             <S.ErrorMessage>Please enter your address.</S.ErrorMessage>
+//           )}
+//         </S.AddressInputBox>
+
+//         <button type="submit">Submit</button>
+//       </form>
+//     </S.PersonalInfoContainer>
+//   );
+// };
+
+// export { PersonalInfo };
--- src/components/booking-system/personal-info/PersonalInfo.styled.jsx
+++ src/components/booking-system/personal-info/PersonalInfo.styled.jsx
@@ -1,6 +1,286 @@
 import styled from "styled-components";
 
-export const PersonalInfoContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+export const PersonalInfoContainer = styled.section`
+  position: relative;
+  width: 100%;
+  background: #fff;
+  padding: 25px;
+  border-radius: 8px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+
+  .container header {
+    font-size: 1.5rem;
+    color: #333;
+    font-weight: 500;
+    text-align: center;
+  }
+  .container .form {
+    margin-top: 30px;
+  }
+  .form .input-box {
+    width: 100%;
+    margin-top: 20px;
+  }
+  .input-box label {
+    color: #333;
+  }
+  .form :where(.input-box input, .select-box) {
+    position: relative;
+    height: 50px;
+    width: 100%;
+    outline: none;
+    font-size: 1rem;
+    color: #707070;
+    margin-top: 8px;
+    border: 1px solid #ddd;
+    border-radius: 6px;
+    padding: 0 15px;
+  }
+  .input-box input:focus {
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
+  }
+  .form .column {
+    display: flex;
+    column-gap: 15px;
+  }
+  .form .gender-box {
+    margin-top: 20px;
+  }
+  .gender-box h3 {
+    color: #333;
+    font-size: 1rem;
+    font-weight: 400;
+    margin-bottom: 8px;
+  }
+  .form :where(.gender-option, .gender) {
+    display: flex;
+    align-items: center;
+    column-gap: 50px;
+    flex-wrap: wrap;
+  }
+  .form .gender {
+    column-gap: 5px;
+  }
+  .gender input {
+    accent-color: rgb(130, 106, 251);
+  }
+  .form :where(.gender input, .gender label) {
+    cursor: pointer;
+  }
+  .gender label {
+    color: #707070;
+  }
+  .address :where(input, .select-box) {
+    margin-top: 15px;
+  }
+  .select-box select {
+    height: 100%;
+    width: 100%;
+    outline: none;
+    border: none;
+    color: #707070;
+    font-size: 1rem;
+  }
+  .form button {
+    height: 55px;
+    width: 100%;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: 400;
+    margin-top: 30px;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease;
+    background: rgb(130, 106, 251);
+  }
+  .form button:hover {
+    background: rgb(88, 56, 250);
+  }
+  /*Responsive*/
+  @media screen and (max-width: 578px) {
+    .form .column {
+      flex-wrap: wrap;
+    }
+    .form :where(.gender-option, .gender) {
+      row-gap: 15px;
+    }
+  }
 `;
+
+// export const Form = styled.form`
+//   margin-top: 30px;
+// `;
+
+// export const Column = styled.div`
+//   display: flex;
+//   column-gap: 15px;
+// `;
+
+// export const InputBox = styled.div`
+//   width: 100%;
+//   margin-top: 20px;
+// `;
+
+// export const Label = styled.label`
+//   color: #333;
+// `;
+
+// export const Input = styled.input`
+//   position: relative;
+//   height: 50px;
+//   width: 100%;
+//   outline: none;
+//   font-size: 1rem;
+//   color: #707070;
+//   margin-top: 8px;
+//   border: 1px solid #ddd;
+//   border-radius: 6px;
+//   padding: 0 15px;
+
+//   &:focus {
+//     box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
+//   }
+// `;
+
+// export const AddressInputBox = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 5px;
+// `;
+
+// export const ErrorInputBox = styled(InputBox)`
+//   border-color: red;
+// `;
+
+// export const ErrorAddressInputBox = styled(AddressInputBox)`
+//   border-color: red;
+// `;
+
+// export const SubmitButton = styled.button`
+//   height: 55px;
+//   width: 100%;
+//   color: #fff;
+//   font-size: 1rem;
+//   font-weight: 400;
+//   margin-top: 30px;
+//   border: none;
+//   cursor: pointer;
+//   transition: all 0.2s ease;
+//   background: rgb(130, 106, 251);
+
+//   &:hover {
+//     background: rgb(88, 56, 250);
+//   }
+// // `;
+// import styled, { css } from "styled-components";
+
+// export const PersonalInfoContainer = styled.section`
+//   position: relative;
+//   width: 100%;
+//   background: #fff;
+//   padding: 25px;
+//   border-radius: 8px;
+//   box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+// `;
+
+// export const InputBox = styled.div`
+//   width: 100%;
+//   margin-top: 20px;
+//   border: 1px solid #ddd;
+//   border-radius: 6px;
+//   padding: 0 15px;
+//   ${(props) =>
+//     props.error &&
+//     css`
+//       border-color: #ff0000;
+//     `}
+// `;
+
+// export const AddressInputBox = styled.div`
+//   margin-top: 20px;
+//   border: 1px solid #ddd;
+//   border-radius: 6px;
+//   padding: 0 15px;
+//   ${(props) =>
+//     props.error &&
+//     css`
+//       border-color: #ff0000;
+//     `}
+// `;
+
+// export const Label = styled.label`
+//   color: #333;
+// `;
+
+// export const Input = styled.input`
+//   position: relative;
+//   height: 50px;
+//   width: 100%;
+//   outline: none;
+//   font-size: 1rem;
+//   color: #707070;
+//   margin-top: 8px;
+//   border: none;
+//   background-color: transparent;
+//   &:focus {
+//     outline: none;
+//   }
+// `;
+
+// export const ErrorMessage = styled.p`
+//   color: #ff0000;
+//   font-style: italic;
+//   margin-top: 5px;
+// `;
+
+// /* Responsive */
+// const columnResponsive = css`
+//   @media screen and (max-width: 578px) {
+//     flex-wrap: wrap;
+//   }
+// `;
+
+// const genderOptionResponsive = css`
+//   @media screen and (max-width: 578px) {
+//     row-gap: 15px;
+//   }
+// `;
+
+// export const Column = styled.div`
+//   display: flex;
+//   column-gap: 15px;
+//   ${columnResponsive}
+// `;
+
+// export const GenderBox = styled.div`
+//   margin-top: 20px;
+//   ${genderOptionResponsive}
+// `;
+
+// export const GenderOption = styled.div`
+//   display: flex;
+//   align-items: center;
+//   column-gap: 50px;
+//   flex-wrap: wrap;
+// `;
+
+// export const Gender = styled.div`
+//   column-gap: 5px;
+// `;
+
+// export const GenderInput = styled.input`
+//   accent-color: rgb(130, 106, 251);
+// `;
+
+// export const GenderLabel = styled.label`
+//   color: #707070;
+// `;
+
+// /* Custom Error Styles */
+// export const ErrorInput = styled(Input)`
+//   border: 1px solid #ff0000;
+// `;
+
+// export const ErrorAddressInputBox = styled(AddressInputBox)`
+//   border: 1px solid #ff0000;
+// `;
